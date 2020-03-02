53ce8c7b095581ef23da85437facddaccb49e741
Daniela Porcescu <dporcescu@grundfos.com>
2020-03-02 15:23:48 +0100

    #156 Adding Dynamic Ingredient Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Auxil from '../../hoc/auxil';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Auxil>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxil>
