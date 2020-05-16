5841adfb288ca1f12652a4e9fdee7e2000b45009
Daniela Porcescu <dporcescu@grundfos.com>
2020-05-16 10:23:47 +0200

    #336 Persistent auth state with localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export function authFail(error) {
 };
 
 export function logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -51,6 +54,10 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 //localId is taken from the console response in browser, this is it's name
                 //response.data is take from azios response and ha sit's propreties like
                 //expiresIn and other that can be passed along
@@ -70,3 +77,22 @@ export function setAuthRedirectPath(path) {
         path: path
     };
 };
+
+export function authCheckState() {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            //what we receive is a string but with new Date it will be converted to a date object
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate > new Date()) {
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+            }
+        }
+    }
+}
